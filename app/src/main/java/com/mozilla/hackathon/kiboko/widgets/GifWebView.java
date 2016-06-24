7840f2c7a1d2d2cc7b4841ee831cb4a7e10b3c66
Brian Mwadime <mwadime@fortunekidew.co.ke>
2016-06-24 12:05:28 +0300

    Adding widget to display gif images.

---
+++ app/src/main/java/com/mozilla/hackathon/kiboko/widgets/GifWebView.java
@@ -0,0 +1,25 @@
+package com.mozilla.hackathon.kiboko.widgets;
+
+import android.content.Context;
+import android.webkit.WebView;
+
+/**
+ * Created by Brian Mwadime on 24/06/2016.
+ */
+
+/**
+ * a WebView is able to do what a browser does.
+ * And since the browser of Android devices using Android 2.2 + supports the animation of GIFs (at least on most devices),
+ * we will use just that
+ *
+ * Sample Usage:
+ * GifWebView view = new GifWebView(this, "file:///android_asset/piggy.gif");
+ * setContentView(view);
+ */
+public class GifWebView extends WebView {
+
+    public GifWebView(Context context, String path) {
+        super(context);
+        loadUrl(path);
+    }
+}
