b6693c0ba49c504488e5bc42e1109c0eed934de3
Brian Mwadime <brian.mwadime@gmail.com>
2016-06-30 02:36:03 +0300

    * Added missing content gifs. * Added LaunchScreen theme. * Updated Icon quiz screen. * Bumped app version.

--- app/src/main/java/com/mozilla/hackathon/kiboko/recievers/DSOBatteryReceiver.java
+++ app/src/main/java/com/mozilla/hackathon/kiboko/recievers/DSOBatteryReceiver.java
@@ -3,6 +3,7 @@ package com.mozilla.hackathon.kiboko.recievers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.BatteryManager;
 
 import com.mozilla.hackathon.kiboko.App;
 import com.mozilla.hackathon.kiboko.events.BatteryStateChanged;
@@ -13,10 +14,15 @@ import com.mozilla.hackathon.kiboko.events.BatteryStateChanged;
 public class DSOBatteryReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        context.unregisterReceiver(this);
+        int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        int level = -1;
+        if (rawlevel >= 0 && scale > 0) {
+            level = (rawlevel * 100) / scale;
+        }
 
-        int level = intent.getIntExtra("level", 0);
-
-        if(level == 0){
+        if(level <= 20){
             App.getBus().post(new BatteryStateChanged(true) );
         }
     }
