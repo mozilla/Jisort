6fe07e2b5741a9f2512b42d52742db8ea98dc129
Brian Mwadime <brian.mwadime@gmail.com>
2016-07-02 12:51:38 +0300

    Merge conflict fix on DSOActivity.java

--- app/src/main/java/com/mozilla/hackathon/kiboko/activities/DSOActivity.java
+++ app/src/main/java/com/mozilla/hackathon/kiboko/activities/DSOActivity.java
@@ -1,17 +1,10 @@
 package com.mozilla.hackathon.kiboko.activities;
 
-import android.support.v7.app.ActionBar;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-
-import com.mozilla.hackathon.kiboko.R;
-
-import com.squareup.otto.Bus;
 
 import com.mozilla.hackathon.kiboko.App;
 import com.mozilla.hackathon.kiboko.events.ApplicationStateChanged;
+import com.squareup.otto.Bus;
 
 /**
  * Created by Brian Mwadime on 06/06/2016.
