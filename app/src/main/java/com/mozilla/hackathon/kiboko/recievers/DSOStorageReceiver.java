faa0919cfca231c38ca40f4bd2a10f449fabe918
Brian Mwadime <brian.mwadime@gmail.com>
2016-07-16 08:42:08 +0300

    Setup local notifications for System events.

--- app/src/main/java/com/mozilla/hackathon/kiboko/recievers/DSOStorageReceiver.java
+++ app/src/main/java/com/mozilla/hackathon/kiboko/recievers/DSOStorageReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.mozilla.hackathon.kiboko.App;
+
 /**
  * Created by mwadime on 6/7/2016.
  */
@@ -11,7 +13,7 @@ public class DSOStorageReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if(intent.getAction().matches(Intent.ACTION_DEVICE_STORAGE_LOW)){
-
+            App.createNotification("Your device is running out of storage.", "storage");
         }
     }
 }
